d1bd88da396afb409e242c494ce711b2133174e9
DevOps@nshu <115215127+anshuvarshney@users.noreply.github.com>
2024-07-12 18:50:00 +0530

    Add files via upload

---
+++ loadscipt.js
@@ -0,0 +1,10 @@
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    script.onload = () => resolve();
+    script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+    document.head.appendChild(script);
+  });
+}
